6cad7d5045102f900a17bc966130e1de3f52d5a0
0B1t322 <dan.demin@gmail.com>
2021-04-12 22:55:21 +0300

    add model project

---
+++ src/pkg/models/project/project.go
@@ -0,0 +1,26 @@
+package project
+
+import (
+	. "github.com/ITLab-Projects/pkg/models/label"
+)
+
+type Project struct {
+	Path			string			`json:"path"`
+	HumanName		string			`json:"humanName"`
+	Description 	string			`json:"description"`
+	LastUpdated		string			`json:"lastUpdated"`
+	Reps			[]string		`json:"reps"`
+	Labels			[]Label			`json:"labels"`
+}
+
+type ProjectInfo struct {
+	Project		Project		`json:"project"`
+	Repos		Meta		`json:"repos"`
+}
+
+type Meta struct {
+	Path			string			`json:"path"`
+	HumanName		string			`json:"humanName"`
+	Description 	string			`json:"description"`
+	Labels			[]Label			`json:"labels"`
+}
