e5e6d32e3a87b65ba7cb303322f80047ee45609f
0B1t322 <dan.demin@gmail.com>
2021-04-12 15:43:52 +0300

    add config package (get it from prev project)

---
+++ src/pkg/config/config.go
@@ -0,0 +1,100 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	log "github.com/sirupsen/logrus"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	DB *DBConfig		`json:"DbOptions"`
+	Auth *AuthConfig	`json:"AuthOptions"`
+	App *AppConfig		`json:"AppOptions"`
+}
+
+type DBConfig struct {
+	URI 					string		`envconfig:"ITLABPROJ_URI",json:"uri"`
+}
+type AuthConfig struct {
+	KeyURL		string		`envconfig:"ITLABPROJ_KEYURL",json:"keyUrl"`
+	Audience	string		`envconfig:"ITLABPROJ_AUDIENCE",json:"audience"`
+	Issuer		string		`envconfig:"ITLABPROJ_ISSUER",json:"issuer"`
+	Scope		string		`envconfig:"ITLABPROJ_SCOPE",json:"scope"`
+	Github		Github		`json:"Github"`
+	Gitlab		Gitlab		`json:"Gitlab"`
+}
+type Github struct {
+	AppID			int64		`json:"appID"`
+	PathToPem		string		`json:"pathToPem"`
+	Installation 	string		`json:"installation"`
+	AccessToken   	string		`envconfig:"ITLABPROJ_GHACCESSTOKEN",json:"accessToken"`
+}
+type Installation struct {
+	ID				int64		`json:"id"`
+	Account     	Account		`json:"account"`
+}
+type Account struct {
+	Login 			string		`json:"login"`
+}
+type Gitlab struct {
+	AccessToken   	string	`json:"accessToken"`
+}
+type AppConfig struct {
+	AppPort				string	`envconfig:"ITLABPROJ_APPPORT",json:"appPort"`
+	TestMode			bool	`envconfig:"ITLABPROJ_TESTMODE",json:"testMode"`
+	ElemsPerPage 		int		`envconfig:"ITLABPROJ_ELEMSPERPAGE",json:"elemsPerPage"`
+	ProjectFileBranch 	string	`envconfig:"ITLABPROJ_PROJFILEBRANCH",json:"projectFileBranch"`
+}
+
+func GetConfig() *Config {
+	var config Config
+	data, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		log.WithFields(
+			log.Fields{
+				"function" : "GetConfig.ReadFile",
+				"error"	:	err,
+			},
+		).Warning("Can't read config.json file, shutting down...")
+	}
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		log.WithFields(
+			log.Fields{
+				"function" : "GetConfig.Unmarshal",
+				"error"	:	err,
+			},
+		).Warning("Can't correctly parse json from config.json, shutting down...")
+	}
+
+	data, err = ioutil.ReadFile("auth_config.json")
+	if err != nil {
+		log.WithFields(
+			log.Fields{
+				"function" : "GetConfig.ReadFile",
+				"error"	:	err,
+			},
+		).Warning("Can't read auth_config.json file, shutting down...")
+	}
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		log.WithFields(
+			log.Fields{
+				"function" : "GetConfig.Unmarshal",
+				"error"	:	err,
+			},
+		).Warning("Can't correctly parse json from auth_config.json, shutting down...")
+	}
+
+	err = envconfig.Process("itlabproj", &config)
+	if err != nil {
+		log.WithFields(
+			log.Fields{
+				"function" : "envconfig.Process",
+				"error"	:	err,
+			},
+		).Fatal("Can't read env vars, shutting down...")
+	}
+	return &config
+}
