9be1a843401b7052a89a5343c0be9a47b4a30249
0B1t322 <dan.demin@gmail.com>
2021-04-13 17:32:58 +0300

    add ReposRepository

---
+++ src/pkg/repositories/repos/reposrepository.go
@@ -0,0 +1,41 @@
+package repos
+
+import (
+	"time"
+	"context"
+
+	"github.com/ITLab-Projects/pkg/models/repo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type ReposRepository struct {
+	repoCollection *mongo.Collection
+}
+
+type ReposRepositorier interface {
+	Save(repos []repo.Repo) error
+}
+
+func New(repoCollection *mongo.Collection) ReposRepositorier {
+	return &ReposRepository{
+		repoCollection: repoCollection,
+	}
+}
+
+func (r *ReposRepository) Save(repos []repo.Repo) error {
+	for _, rep := range repos {
+		opts := options.Replace().SetUpsert(true)
+		filter := bson.M{"id": rep.ID}
+		
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		_, err := r.repoCollection.ReplaceOne(ctx, filter, rep, opts)
+		// TODO think about should we return err or just print about it
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
