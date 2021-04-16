d664557c422cf5e17feee0cd4660e300af1cfaae
0B1t322 <dan.demin@gmail.com>
2021-04-16 20:25:24 +0300

    update tests

--- src/pkg/repositories/repositories_test.go
+++ src/pkg/repositories/repositories_test.go
@@ -1,16 +1,22 @@
 package repositories_test
 
 import (
+	"os"
+	"sync"
 	"testing"
 
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+
 	"github.com/ITLab-Projects/pkg/githubreq"
+	"github.com/ITLab-Projects/pkg/models/milestone"
 	"github.com/ITLab-Projects/pkg/models/repo"
 
 	"github.com/ITLab-Projects/pkg/repositories"
 )
 
-
 var Repositories *repositories.Repositories
+var requster *githubreq.GHRequester
 
 func init() {
 	_r, err := repositories.New(&repositories.Config{
@@ -21,20 +27,85 @@ func init() {
 	}
 
 	Repositories = _r
+
+	if err := godotenv.Load("../../.env"); err != nil {
+		panic(err)
+	}
+
+	token, find := os.LookupEnv("ITLAB_PROJECTS_ACCESSKEY")
+	if !find {
+		panic("Don't find token")
+	}
+
+	requster = githubreq.New(
+		&githubreq.Config{
+			AccessToken: token,
+		},
+	)
+
+	logrus.Info(token)
 }
 
-func TestFunc_Save(t *testing.T) {
-	r := githubreq.New(&githubreq.Config{
-		AccessToken: "",
-	})
-	repos, err := r.GetRepositories()
+func TestFunc_SaveRepo(t *testing.T) {
+	repos, err := requster.GetRepositories()
 	if err != nil {
 		t.Log(err)
 		t.FailNow()
 	}
 
-	if err := Repositories.Save(repo.ToRepo(repos)); err != nil {
+	if err := Repositories.ReposRepositorier.Save(repo.ToRepo(repos)); err != nil {
 		t.Log(err)
 		t.FailNow()
 	}
+}
+
+func TestFunc_GetAllRepos(t *testing.T) {
+	var repos []repo.Repo
+	err := Repositories.ReposRepositorier.GetAll(&repos)
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	if len(repos) == 0 {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	t.Log(len(repos))
+}
+
+func TestFunc_SaveMilestones(t *testing.T) {
+	repos, err := requster.GetRepositories()
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	var wg sync.WaitGroup
+	for i, _ := range repos {
+		wg.Add(1)
+		go func(r *repo.RepoWithURLS, wg *sync.WaitGroup) {
+			defer wg.Done()
+			ms, err := requster.GetMilestonesForRepoWithID(r.Repo)
+			if err != nil {
+				t.Log(err)
+			}
+			if err := Repositories.Milestoner.Save(ms); err != nil {
+				t.Log(err)
+			}
+		}(&repos[i], &wg)
+	}
+	wg.Wait()
+}
+
+func TestFunc_GetAllMilestones(t *testing.T) {
+	var ms []milestone.MilestoneInRepo
+	err := Repositories.Milestoner.GetAll(&ms)
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	t.Log(len(ms))
 }
