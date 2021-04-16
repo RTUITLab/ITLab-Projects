592d9119d434db3992f6547db4dc2b19672f3c03
0B1t322 <dan.demin@gmail.com>
2021-04-16 20:21:45 +0300

    update repo model

--- src/pkg/models/repo/repo.go
+++ src/pkg/models/repo/repo.go
@@ -1,32 +1,22 @@
 package repo
 
 import (
-	. "github.com/ITLab-Projects/pkg/models/project"
 	. "github.com/ITLab-Projects/pkg/models/user"
 )
 
 type Repo struct {
 	ID          		uint64 		`json:"id"`
-	Platform			string		`json:"platform,omitempty"`
 	Name        		string 		`json:"name"`
 	Contributors		[]User		`json:"contributors"`
-	Path				string		`json:"path_with_namespace,omitempty"`
+	// Path				string		`json:"path_with_namespace,omitempty"`
 	HTMLUrl     		string 		`json:"html_url"`
-
-	// GitLabHTMLUrl     	string 		`json:"web_url,omitempty"`
-
 	Description 		string 		`json:"description"`
 	CreatedAt   		string 		`json:"created_at"`
 	UpdatedAt   		string 		`json:"updated_at"`
 	PushedAt			string		`json:"pushed_at"`
-
-	// GitLabUpdatedAt   	string 		`json:"last_activity_at,omitempty"`
-
 	Language    		string 		`json:"language"`
 	Languages			map[string]int	`json:"languages"`
 	Archived    		bool   		`json:"archived"`
-	OpenIssues  		int			`json:"open_issues_count"`
-	Meta				Meta		`json:"meta"`
 }
 
 type RepoWithURLS struct {
