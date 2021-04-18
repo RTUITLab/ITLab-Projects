8bb529db32ba4af4df78d0e66595deba63084e15
0B1t322 <dan.demin@gmail.com>
2021-04-18 17:18:14 +0300

    update repo repository

--- src/pkg/repositories/repos/repo.go
+++ src/pkg/repositories/repos/repo.go
@@ -1,15 +1,15 @@
 package repos
 
 import (
-	"github.com/ITLab-Projects/pkg/repositories/getter"
-	"github.com/ITLab-Projects/pkg/repositories/counter"
-	"fmt"
 	"context"
-	"errors"
 	"time"
 
+	"github.com/ITLab-Projects/pkg/repositories/counter"
+	"github.com/ITLab-Projects/pkg/repositories/getter"
+	"github.com/ITLab-Projects/pkg/repositories/saver"
+	"github.com/ITLab-Projects/pkg/repositories/typechecker"
+
 	"github.com/ITLab-Projects/pkg/models/repo"
-	wrapper "github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -20,6 +20,7 @@ type ReposRepository struct {
 	CountOfDocuments	int64
 	counter.Counter
 	getter.Getter
+	saver.Saver
 }
 
 
@@ -29,32 +30,24 @@ func New(repoCollection *mongo.Collection) ReposRepositorier {
 		Counter: counter.New(repoCollection),
 	}
 
+	Type := repo.Repo{}
+
 	rr.Getter = getter.New(
 		repoCollection,
-		rr.checkType,	
+		typechecker.NewSingleByInterface(Type),
+	)
+
+	rr.Saver = saver.New(
+		repoCollection,
+		Type,
+		rr.save,
 	)
 
 	return rr
 }
 
 func (r *ReposRepository) Save(repos interface{}) error {
-	var err error = nil
-	switch repos.(type) {
-	case []repo.Repo:
-		err = r.saveAll(repos.([]repo.Repo))
-	case repo.Repo:
-		err = r.save(repos.(repo.Repo))
-	case *repo.Repo:
-		err = r.save(*(repos.(*repo.Repo)))
-	default:
-		err = wrapper.Wrapf(
-			errors.New("Uknown type"), 
-			"%T Expected %T or %T or %T", 
-			repos, []repo.Repo{}, repo.Repo{}, &repo.Repo{}, 
-		)
-	}
-	
-	if err != nil {
+	if err := r.Saver.Save(repos); err != nil {
 		return err
 	}
 
@@ -75,7 +68,9 @@ func (r *ReposRepository) saveAll(repos []repo.Repo) error {
 	return nil
 }
 
-func (r *ReposRepository) save(repo repo.Repo) error {
+func (r *ReposRepository) save(v interface{}) error {
+	repo, _ := v.(repo.Repo)
+	
 	opts := options.Replace().SetUpsert(true)
 	filter := bson.M{"id": repo.ID}
 	
@@ -86,19 +81,5 @@ func (r *ReposRepository) save(repo repo.Repo) error {
 	}
 
 	return nil
-}
-
-
-func (r *ReposRepository) checkType(v interface{}) error {
-	var err error = nil
-
-	switch v.(type) {
-	case *[]repo.Repo:
-		break
-	default:
-		err = fmt.Errorf("Uknown type: %T Expected: %T", v, &[]repo.Repo{})
-	}
-
-	return err
 }
 
