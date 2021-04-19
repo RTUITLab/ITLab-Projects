c79c033d9c99d5a763bfd558f3529ef9fd26dfb4
0B1t322 <dan.demin@gmail.com>
2021-04-20 00:07:55 +0300

    move app

---
+++ src/app/app.go
@@ -0,0 +1,63 @@
+package app
+
+import (
+	"github.com/ITLab-Projects/pkg/githubreq"
+	"net/http"
+	"github.com/ITLab-Projects/pkg/apibuilder"
+	log "github.com/sirupsen/logrus"
+	"github.com/ITLab-Projects/pkg/config"
+	"github.com/ITLab-Projects/pkg/repositories"
+
+	"github.com/gorilla/mux"
+)
+
+type App struct {
+	Router 	*mux.Router
+	Repository *repositories.Repositories
+	Requester githubreq.Requester
+	Port 	string
+}
+
+func New(cfg *config.Config) *App {
+	app := &App{}
+
+	app.Port = cfg.App.AppPort
+
+	if _rep, err := repositories.New(&repositories.Config{
+		DBURI: cfg.DB.URI,
+	}); err != nil {
+		log.WithFields(
+			log.Fields{
+				"package": "app",
+				"func": "New",
+				"err": err,
+			},
+		).Panic("Failed to init App")
+	} else {
+		app.Repository = _rep
+	}
+
+	app.Requester = githubreq.New(&githubreq.Config{
+		AccessToken: cfg.Auth.Github.AccessToken,
+	})
+
+	app.Router = mux.NewRouter()
+
+	return app
+}
+
+func (a *App) AddApi(Builders ...apibuilder.ApiBulder) {
+	for _, Builder := range Builders {
+		Builder.Build(a.Router)
+	}
+}
+
+func (a *App) Start() {
+	log.Infof("Starting Application is port %s", a.Port)
+	if err := http.ListenAndServe(a.Port, a.Router); err != nil {
+		log.Panicf("Failed to start application %v", err)
+	}
+}
+
+
+
