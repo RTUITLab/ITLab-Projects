a898688e5d7c7f42b96e059ce18612ffc1417a5d
0B1t322 <dan.demin@gmail.com>
2021-04-22 08:54:14 +0300

    add errors

--- src/pkg/githubreq/errors.go
+++ src/pkg/githubreq/errors.go
@@ -7,4 +7,6 @@ import (
 var (
 	UnexpectedCode = errors.New("UnexpectedCode")
 	ErrGetLastPage = errors.New("Can't get last page")
+	ErrForbiden = errors.New("Forbidden status from github")
+	ErrUnatorizared = errors.New("Unathorizeted status from github")
 )
