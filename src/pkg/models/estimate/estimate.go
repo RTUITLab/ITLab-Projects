3f6d7def7e837b534f04709b98450c9ac299e4f2
0B1t322 <dan.demin@gmail.com>
2021-04-22 17:51:12 +0300

    refactor models

--- src/pkg/models/estimate/estimate.go
+++ src/pkg/models/estimate/estimate.go
@@ -1,6 +1,6 @@
 package estimate
 
 type Estimate struct {
-	MilestoneID	uint	`json:"milestone_id" bson:"milestone_id"`
+	MilestoneID	uint64	`json:"milestone_id" bson:"milestone_id"`
 	EstimateURL	string	`json:"estimate_url" bson:"estimate_url"`
 }
--- src/pkg/models/functask/functask.go
+++ src/pkg/models/functask/functask.go
@@ -1,6 +1,6 @@
 package functask
 
 type FuncTask struct {
-	MilestoneID	uint	`json:"milestone_id" bson:"milestone_id"`
+	MilestoneID	uint64	`json:"milestone_id" bson:"milestone_id"`
 	FuncTaskURL	string	`json:"func_task_url" bson:"func_task_url"`
 }
