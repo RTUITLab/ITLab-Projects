cc320b6332da0553925f0f921b3e08a59e5a3882
0B1t322 <dan.demin@gmail.com>
2021-04-24 19:34:46 +0300

    add compact model

--- src/pkg/models/repoasproj/repoasproj.go
+++ src/pkg/models/repoasproj/repoasproj.go
@@ -1,6 +1,8 @@
 package repoasproj
 
 import (
+	"time"
+
 	"github.com/ITLab-Projects/pkg/models/milestone"
 	"github.com/ITLab-Projects/pkg/models/realese"
 	"github.com/ITLab-Projects/pkg/models/repo"
@@ -12,4 +14,29 @@ type RepoAsProj struct {
 	Milestones 		[]milestone.Milestone		`json:"milestones"`
 	LastRealese		*realese.Realese			`json:"last_realese"`
 	Tags			[]tag.Tag					`json:"tags"`
+}
+
+
+type RepoAsProjCompact struct {
+	Repo 			repo.Repo					`json:"repo"`
+	Completed		float64						`json:"completed"`
+	Tags			[]tag.Tag					`json:"tags"`
+}
+
+type ByCreateDate []RepoAsProjCompact
+
+func (b ByCreateDate) Len() int {
+	return len(b)
+}
+
+func (b ByCreateDate) Less(i, j int) bool {
+	parsedF, _ := time.Parse(time.RFC3339, b[i].Repo.CreatedAt)
+	parsedS, _ := time.Parse(time.RFC3339, b[j].Repo.CreatedAt)
+
+	return parsedF.After(parsedS)
+
+}
+
+func (b ByCreateDate) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
 }
