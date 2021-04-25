9cad9ea642acdebcf2e6e62ab474b67668c36705
0B1t322 <dan.demin@gmail.com>
2021-04-25 19:43:31 +0300

    add saver with update

--- src/pkg/repositories/saver/save.go
+++ src/pkg/repositories/saver/save.go
@@ -80,6 +80,25 @@ func NewSaver(
 	return s
 }
 
+func (s *Save) makeSliceOfValue(value reflect.Value) interface{} {
+	if value.Type().AssignableTo(
+		reflect.PtrTo(s.t),
+	) {
+		value = value.Elem()
+	}
+
+	if value.Type().AssignableTo(
+		s.t,
+	) {
+		slice := reflect.MakeSlice(s.t, 0, 1)
+		slice = reflect.Append(slice, value)
+
+		return slice.Interface()
+	}
+
+	return value.Interface()
+}
+
 func getTypeOrPanic(Type interface{}) (reflect.Type, error) {
 	t := reflect.TypeOf(Type)
 	switch t.Kind() {
@@ -156,3 +175,96 @@ func (swd *SaveWithDelete) SaveAndDeletedUnfind(ctx context.Context, v interface
 func (swd *SaveWithDelete) Save(v interface{}) error {
 	return swd.s.Save(v)
 }
+
+type SaveWithUpdate struct {
+	s *Save
+	f filterBuilder
+	Saver
+}
+
+func NewSaveWithUpdate(
+	collection *mongo.Collection,
+	Type interface{},
+	saveFunc saveWithReplaceFunc,
+	f filterBuilder,
+) *SaveWithUpdate {
+	s := &SaveWithUpdate{
+		f: f,
+	}
+
+	s.s = NewSaver(
+		collection,
+		Type,
+		saveFunc,
+	)
+
+	s.Saver = s.s
+
+	return s
+}
+
+func(swu *SaveWithUpdate) SaveAndUpdatenUnfind(
+	ctx context.Context, 
+	v interface{},	// value that we  
+	updateFilter interface{},	// filter where you change field
+) error {
+	if err := swu.s.Save(v); err != nil {
+		return err
+	}
+
+	filter := swu.f(
+		swu.s.makeSliceOfValue(
+			reflect.ValueOf(v),
+		),
+	)
+
+	if _, err := swu.s.collection.UpdateMany(
+		ctx,
+		filter,
+		updateFilter,
+		options.Update(),
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+
+type SaveWithDelUpdate struct {
+	SaverWithDelUpdate
+}
+
+func NewSaverWithDelUpdate(
+	collection *mongo.Collection,
+	Type interface{},
+	saveFunc saveWithReplaceFunc,
+	f filterBuilder,
+) *SaveWithDelUpdate {
+	s := &SaveWithDelUpdate{}
+
+	type _SaveWithDelUpdate struct {
+		*SaveWithDelete
+		*SaveWithUpdate
+	}
+
+	_s := &_SaveWithDelUpdate{
+		SaveWithDelete: NewSaverWithDelete(
+			collection,
+			Type,
+			saveFunc,
+			f,
+		),
+		SaveWithUpdate: NewSaveWithUpdate(
+			collection,
+			Type,
+			saveFunc,
+			f,
+		),
+	}
+	s.SaverWithDelUpdate = _s
+
+	return s
+}
+
+
--- src/pkg/repositories/saver/saver.go
+++ src/pkg/repositories/saver/saver.go
@@ -12,3 +12,18 @@ type SaverWithDelete interface {
 	Saver
 	SaveAndDeletedUnfind(context.Context, interface{}) error
 }
+
+type SaverWithUpdate interface {
+	Saver
+	SaveAndUpdatenUnfind(
+		ctx context.Context, 
+		filter interface{},	// value that we  
+		updateFilter interface{},	// filter where you change field
+	) error
+}
+
+
+type SaverWithDelUpdate interface {
+	SaverWithDelete
+	SaverWithUpdate
+}
