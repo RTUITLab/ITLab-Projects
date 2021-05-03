3493c53cdee6b313aaf7a0d2ba5e20bf92e1699a
0B1t322 <dan.demin@gmail.com>
2021-05-03 17:27:27 +0300

    refactor context - add timeout

--- src/ITLab-Projects/service/api/v1/tags.go
+++ src/ITLab-Projects/service/api/v1/tags.go
@@ -1,10 +1,12 @@
 package v1
 
 import (
-	e "github.com/ITLab-Projects/pkg/err"
 	"context"
 	"encoding/json"
 	"net/http"
+	"time"
+
+	e "github.com/ITLab-Projects/pkg/err"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -13,26 +15,31 @@ import (
 )
 
 // GetTags
-// 
+//
 // @Summary return all tags
-// 
+//
 // @Produce json
-// 
+//
 // @Tags tags
-// 
+//
 // @Description return all tags
-// 
+//
 // @Router /api/v1/projects/tags [get]
-// 
+//
 // @Success 200 {array} tag.Tag
-// 
+//
 // @Failure 500 {object} e.Message
-// 
-// @Failure 401 {object} e.Message 
+//
+// @Failure 401 {object} e.Message
 func (a *Api) GetTags(w http.ResponseWriter, r *http.Request) {
 	var tags []tag.Tag
 
-	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		1*time.Second,
+	)
+	defer cancel()
+	
 	if err := a.Repository.Tag.GetAllFiltered(
 		ctx,
 		bson.M{},
