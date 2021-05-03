349a16227e446160784979a1a7c208ce0b44de0f
0B1t322 <dan.demin@gmail.com>
2021-05-03 17:50:55 +0300

    add bench test

---
+++ src/ITLab-Projects/main_test.go
@@ -0,0 +1,34 @@
+package main_test
+
+import (
+	"log"
+	"net/http"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestFubc_Bench(t *testing.T) {
+	req, err := http.NewRequest("GET", "http://127.0.0.1:8080/api/v1/projects/issues", nil)
+	if err != nil {
+		t.Log(err)
+	}
+	wg := sync.WaitGroup{}
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func(t *testing.T, req *http.Request, wg *sync.WaitGroup) {
+			time.Sleep(10 * time.Millisecond)
+			defer wg.Done()
+			log.Println("Gor start")
+			if resp, err := http.DefaultClient.Do(req); err != nil {
+				log.Println(err)
+			} else {
+				log.Println(resp.StatusCode)
+			}
+		}(t, req, &wg)
+	}
+	wg.Wait()
+
+	t.Log("DOne")
+
+}
