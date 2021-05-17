23cfdd9769c59f76e0b6b570da726a0ccbb9b941
0B1t322 <dan.demin@gmail.com>
2021-05-17 10:42:24 +0300

    func tp update and get projects

--- src/ITLab-Projects/service/api/v1/projects/logic.go
+++ src/ITLab-Projects/service/api/v1/projects/logic.go
@@ -1,9 +1,13 @@
 package projects
 
 import (
-	"errors"
-	"github.com/ITLab-Projects/pkg/models/repo"
 	"context"
+	"errors"
+	"strings"
+
+	"github.com/ITLab-Projects/pkg/models/repo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/go-kit/kit/log/level"
 
@@ -34,40 +38,88 @@ func (s *service) GetProject(
 }
 
 func (s *service) GetProjects(
-	ctx context.Context, 
-	start int64, 
-	count int64,
-) ([]*repoasproj.RepoAsProjCompact, error) {
-	panic("not implemented") // TODO: Implement
+	ctx 			context.Context, 
+	start, 	count 	int64,
+	name, 	tag		string,
+) ([]*repoasproj.RepoAsProjCompactPointers, error) {
+	filter, err := s.buildFilterForGetProject(
+		ctx,
+		name,
+		tag,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	repos, err := s.repository.GetFiltrSortFromToRepos(
+		ctx,
+		filter,
+		bson.D{ {"createdat", -1}, {"deleted", 1}},
+		start,
+		count,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	projs, err := s.GetCompatcProj(ctx, repos)
+	if err != nil {
+		return nil, err
+	}
+
+	return projs, nil
 }
 
-// func (s *service) UpdateProjects(
-// 	ctx context.Context,
-// ) error {
-// 	s.resetUpdater()
-// 	defer s.resetUpdater()
+func (s *service) UpdateProjects(
+	ctx context.Context,
+) error {
+	s.resetUpdater()
+	defer s.resetUpdater()
 
-// 	repos, ms, rs, tgs, err := s.getAllFromGithub(ctx)
-// 	if err != nil {
-// 		return err
-// 	}
+	repos, ms, rs, tgs, err := s.getAllFromGithub(ctx)
+	if err != nil {
+		return err
+	}
 
-// 	if err := s.repository.SaveReposAndSetDeletedUnfind(
-// 		ctx,
-// 		repos,
-// 	); err != nil {
-// 		return err
-// 	}
+	if err := s.repository.SaveReposAndSetDeletedUnfind(
+		ctx,
+		repos,
+	); err != nil {
+		return err
+	}
 
-// 	if err := s.repository.SaveMilestonesAndSetDeletedUnfind(
-// 		ctx,
-// 		ms,
-// 	); err != nil {
-// 		return err
-// 	}
+	if err := s.repository.SaveMilestonesAndSetDeletedUnfind(
+		ctx,
+		ms,
+	); err != nil {
+		return err
+	}
 
+	is := getIssuesFromMilestone(ms)
 
-// }
+	if err := s.repository.SaveIssuesAndSetDeletedUnfind(
+		ctx,
+		is,
+	); err != nil {
+		return err
+	}
+
+	if err := s.repository.SaveRealeses(
+		ctx,
+		rs,
+	); err != nil {
+		return err
+	}
+
+	if err := s.repository.SaveAndDeleteUnfindTags(
+		ctx,
+		tgs,
+	); err != nil {
+		return nil
+	}
+
+	return nil
+}
 
 func (s *service) DeleteProject(
 	ctx context.Context, 
@@ -193,4 +245,191 @@ func (s *service) getAllFromGithub(
 	close(tgsChan)
 
 	return repo.ToRepo(repos) ,ms, rs, tgs, nil
+}
+
+func getIssuesFromMilestone(
+	ms []milestone.MilestoneInRepo,
+) []milestone.IssuesWithMilestoneID {
+	var is []milestone.IssuesWithMilestoneID
+
+	isChan := make(chan milestone.IssuesWithMilestoneID)
+
+	count := 0
+	for j, _ := range ms {
+		for i, _ := range ms[j].Issues {
+			count++
+			go func(i milestone.Issue, MID , RepoID uint64) {
+				isChan <- milestone.IssuesWithMilestoneID{
+					MilestoneID: MID,
+					RepoID: RepoID,
+					Issue: i,
+				}
+			}(ms[j].Issues[i], ms[j].Milestone.ID, ms[j].RepoID)
+		}
+	}
+
+	for i := 0; i < count; i++ {
+		select{
+		case issue := <- isChan:
+			is = append(is, issue)
+		}
+	}
+
+	return is
+}
+
+func (s *service) buildFilterForGetProject(
+	ctx 		context.Context,
+	name, tag 	string,
+) (interface{}, error) {
+	filter := bson.M{}
+
+	if name != "" {
+		if err := s.buildNameFilterForGetProjects(
+			ctx,
+			name,
+			&filter,
+		); err != nil {
+			return nil, err
+		}
+	}
+
+	if tag != "" {
+		if err := s.buildTagFilterForGetProjects(
+			ctx,
+			tag,
+			&filter,
+		); err != nil {
+			return nil, err
+		}
+	}
+
+	return filter, nil
+}
+
+func (s *service) buildTagFilterForGetProjects(
+	ctx 	context.Context,
+	t 		string,
+	filter	*bson.M,
+) (error) {
+	massOfTags := strings.Split(t, " ")
+
+	tags, err := s.repository.GetFilteredTags(
+		ctx,
+		bson.M{"tag": bson.M{"$in": massOfTags}},
+	)
+	if err != nil {
+		return err
+	}
+
+	var ids []uint64
+	for _, t := range tags {
+		ids = append(ids, t.RepoID)
+	}
+
+	(map[string]interface{})(*filter)["id"] = bson.M{"$in": ids}
+
+	return nil
+}
+
+func (s *service) buildNameFilterForGetProjects(
+	_ context.Context,
+	name string,
+	filter *bson.M,
+) error {
+	(map[string]interface{})(*filter)["name"] = bson.M{"$regex": name, "$options": "-i"}
+
+	return nil
+}
+
+func (s *service) GetCompatcProj(
+	ctx context.Context,
+	repos []*repo.Repo,
+) ([]*repoasproj.RepoAsProjCompactPointers, error) {
+	ctx, cancel := context.WithCancel(
+		ctx,
+	)
+	defer cancel()
+
+	errChan := make(chan error, 1)
+	projChan := make(chan *repoasproj.RepoAsProjCompactPointers, 1)
+
+	var count uint
+	for i := range repos {
+		count++
+		go func(r *repo.Repo) {
+			defer s.catchPanic()
+			proj := &repoasproj.RepoAsProjCompactPointers{
+				Repo: r,
+			}
+
+			if ms, err := s.repository.GetAllByRepoID(
+				ctx,
+				r.ID,
+			); err == mongo.ErrNoDocuments {
+				// Pass
+			} else if err != nil {
+				cancel()
+				errChan <- err
+				return
+			} else {
+				proj.Completed = countCompleted(ms)
+			}
+
+			tgs, err := s.repository.GetFilteredTagsByRepoID(
+				ctx,
+				r.ID,
+			)
+			if err == mongo.ErrNoDocuments {
+				// Pass
+			} else if err != nil {
+				cancel()
+				errChan <- err
+				return
+			} else {
+				proj.Tags = tgs
+			}
+
+			projChan <- proj
+		}(repos[i])
+	}
+
+	var projs []*repoasproj.RepoAsProjCompactPointers
+
+	for i := uint(0); i < count; i++ {
+		select{
+		case p := <- projChan:
+			projs = append(projs, p)
+		case <-ctx.Done():
+			err := <- errChan
+			defer close(errChan)
+			defer close(projChan)
+			return nil, err
+		}
+	}
+
+	return projs, nil
+}
+
+func (s *service) catchPanic() {
+	if r := recover(); r != nil {
+		level.Debug(s.logger).Log("CatchPanic", r)
+	}
+}
+
+func countCompleted(ms []*milestone.MilestoneInRepo) float64 {
+	var open float64
+	var closed float64
+	for _, m := range ms {
+		if m.OpenIssues != 0 {
+			open += float64(m.OpenIssues)
+			closed += float64(m.ClosedIssues)
+		}
+	}
+
+	if open + closed == 0 {
+		return 1
+	} else {
+		return (closed)/(open+closed)
+	}
 }
