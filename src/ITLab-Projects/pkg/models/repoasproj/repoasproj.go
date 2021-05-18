3e78856205ed10af5a68ef9dd4ba97936bb4a681
0B1t322 <dan.demin@gmail.com>
2021-05-18 15:36:11 +0300

    add models

--- src/ITLab-Projects/pkg/models/repoasproj/repoasproj.go
+++ src/ITLab-Projects/pkg/models/repoasproj/repoasproj.go
@@ -17,6 +17,14 @@ type RepoAsProj struct {
 	Completed		float64						`json:"completed"`
 }
 
+type RepoAsProjPointer struct {
+	Repo 			*repo.Repo 					`json:"repo"`
+	Milestones 		[]*milestone.Milestone		`json:"milestones"`
+	LastRealese		*realese.Realese			`json:"last_realese"`
+	Tags			[]*tag.Tag					`json:"tags"`
+	Completed		float64						`json:"completed"`
+}
+
 
 type RepoAsProjCompact struct {
 	Repo 			repo.Repo					`json:"repo"`
@@ -52,4 +60,28 @@ type RepoAsProjCompactPointers struct {
 	Repo 			*repo.Repo					`json:"repo"`
 	Completed		float64						`json:"completed"`
 	Tags			[]*tag.Tag					`json:"tags"`
+}
+
+type ByCreateDatePointers []*RepoAsProjCompactPointers
+
+func (b ByCreateDatePointers) Len() int {
+	return len(b)
+}
+
+func (b ByCreateDatePointers) Less(i, j int) bool {
+	if b[i].Repo.Deleted {
+		return false
+	} else if b[j].Repo.Deleted {
+		return true
+	}
+
+	parsedF, _ := time.Parse(time.RFC3339, b[i].Repo.CreatedAt)
+	parsedS, _ := time.Parse(time.RFC3339, b[j].Repo.CreatedAt)
+
+	return parsedF.After(parsedS)
+
+}
+
+func (b ByCreateDatePointers) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
 }
