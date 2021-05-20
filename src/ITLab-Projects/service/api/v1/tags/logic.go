a68fa98e3143b45833f3ee6d9a358b52a206299f
0B1t322 <dan.demin@gmail.com>
2021-05-20 20:09:21 +0300

    add errors and return in getTags method

--- src/ITLab-Projects/service/api/v1/tags/logic.go
+++ src/ITLab-Projects/service/api/v1/tags/logic.go
@@ -2,11 +2,18 @@ package tags
 
 import (
 	"context"
+	"errors"
+	"net/http"
 
 	"github.com/ITLab-Projects/pkg/models/tag"
+	"github.com/ITLab-Projects/pkg/statuscode"
 	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/go-kit/kit/log"
+)
+
+var (
+	ErrGetTags		= errors.New("Faield to get tags")
 )
 
 type service struct {
@@ -27,13 +34,18 @@ func New(
 func (s *service) GetAllTags(
 	ctx context.Context,
 ) ([]*tag.Tag, error) {
+	logger := log.With(s.logger, "method", "GetAllTags")
 	tgs, err := s.repository.GetAllTags(
 		ctx,
 	)
 	if err == mongo.ErrNoDocuments {
 		// Pass
 	} else if err != nil {
-		return nil, err
+		logger.Log("Failed to get tags: err", err)
+		return nil, statuscode.WrapStatusError(
+			ErrGetTags,
+			http.StatusInternalServerError,
+		)
 	}
 
 	return tgs, nil
